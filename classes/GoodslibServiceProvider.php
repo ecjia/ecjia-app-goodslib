96c2409042759032b97a26f65722914ce626cc7e
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-30 23:36:01 +0800

    指定ServiceProvider的路径

--- classes/GoodslibServiceProvider.php
+++ classes/GoodslibServiceProvider.php
@@ -9,7 +9,7 @@ class GoodslibServiceProvider extends  AppParentServiceProvider
     
     public function boot()
     {
-        $this->package('ecjia/app-goodslib');
+        $this->package('ecjia/app-goodslib', null, dirname(__DIR__));
     }
     
     public function register()
