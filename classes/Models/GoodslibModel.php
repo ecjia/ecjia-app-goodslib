01b6ca4bb88f6797312f9f1a77332501264779db
王正东 <huangyuyuan@ecmoban.comwhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comdhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-04-26 18:22:18 +0800

    添加商品库的数据模型

---
+++ classes/Models/GoodslibModel.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: royalwang
+ * Date: 2019-02-19
+ * Time: 18:16
+ */
+
+namespace Ecjia\App\Goodslib\Models;
+
+use Royalcms\Component\Database\Eloquent\Model;
+
+class GoodslibModel extends Model
+{
+
+    protected $table = 'goodslib';
+
+    protected $primaryKey = 'goods_id';
+
+    /**
+     * 可以被批量赋值的属性。
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'cat_id',
+        'cat_level1_id',
+        'cat_level2_id',
+        'goods_sn',
+        'goods_barcode',
+        'goods_name',
+        'goods_name_style',
+        'brand_id',
+        'provider_name',
+        'goods_weight',
+        'weight_unit',
+        'market_price',
+        'shop_price',
+        'generate_date',
+        'expiry_date',
+        'limit_days',
+        'keywords',
+        'pinyin_keyword',
+        'goods_brief',
+        'goods_desc',
+        'goods_thumb',
+        'goods_img',
+        'original_img',
+        'is_real',
+        'extension_code',
+        'add_time',
+        'sort_order',
+        'is_delete',
+        'last_update',
+        'goods_type',
+        'review_status',
+        'review_content',
+        'goods_rank',
+        'is_display',
+        'used_count',
+    ];
+
+    /**
+     * 该模型是否被自动维护时间戳
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    
+    /**
+     * 商品货品信息
+     */
+    public function products()
+    {
+    	return $this->hasMany('Ecjia\App\Cart\Models\ProductsModel', 'goods_id', 'goods_id');
+    }
+    
+    /**
+     * 商品属性信息
+     */
+    public function goods_attr()
+    {
+    	return $this->hasMany('Ecjia\App\Goodslib\Models\GoodsAttrModel', 'goods_id', 'goods_id');
+    }
+
+}
