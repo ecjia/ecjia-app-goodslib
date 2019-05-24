dfbd13a78046905db8a1f2608f72857f3ce00ebd
宋倩倩 <huangyuyuan@ecmoban.comshuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comqhuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2019-05-24 17:19:26 +0800

    当规格并没有设置规格属性时候增加判断

--- templates/admin/select_spec_values.dwt.php
+++ templates/admin/select_spec_values.dwt.php
@@ -6,6 +6,7 @@
 </div> 
 
 <div class="modal-body">
+	{if $goods_attr_html}
 	 <form class="form-horizontal" method="post" name="insertForm" id="insertForm" action='{url path="goodslib/admin/select_spec_values_insert"}'>
 		<div id="tbody-goodsAttr"> 
 			{if $goods_attr_html}{$goods_attr_html}{/if}
@@ -19,4 +20,20 @@
 			</div>
 		</div>
 	</form>
+	{else}
+	 <form class="form-horizontal" method="post" name="insertForm" id="insertForm" action='{url path="goodslib/admin/select_spec_values_insert"}'>
+		<div class="control-group">
+			<label class="control-label"></label>
+			<div class="controls l_h35">
+				<i class="fontello-icon-attention-circled ecjiafc-red"></i><span class="ecjiafc-red">您当前绑定的模板还未设置规格属性。</span>
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<div class="controls l_h35">
+				<a href='{url path="goods/admin_spec_attribute/init" args="cat_id={$template_id}"}'><button type="button" class="btn btn-info" >{t domain="goodslib"}查看{/t}</button></a>
+			</div>
+		</div>
+	</form>
+	{/if}
 </div>
